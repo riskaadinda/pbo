85069d1a636e24bd83d4e3afc326cd59170c7532
riskaadinda <84611327+riskaadinda@users.noreply.github.com>
2021-06-16 11:14:45 +0700

    Add files via upload

---
+++ ganjill.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rindaa.program;
+
+/**
+ *
+ * @author USER
+ */
+public class ganjill {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka = 21;
+        if (angka %2 == 0)
+            System.out.println(angka + " adalah bilangan genap ");
+        else 
+         System.out.println(angka + " adalah bilangan ganjil ");
+    }
+    
+}
---
+++ genap.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rindaa.program;
+
+/**
+ *
+ * @author USER
+ */
+public class ganjil_genap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka = 10;
+        if (angka %2 == 0)
+            System.out.println(angka + " adalah bilangan genap ");
+        else 
+         System.out.println(angka + " adalah bilangan ganjil ");
+    }
+}
